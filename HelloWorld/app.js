de5ffbfa499117dbf2717ea974732e9b20b39334
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-01-08 23:24:25 +0530

    First Node JS Program

---
+++ HelloWorld/app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostName = '127.0.0.1';
+const portNumber = 3003;
+
+const server = http.createServer( (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Node JS World\n');
+} );
+
+server.listen( portNumber, hostName, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
