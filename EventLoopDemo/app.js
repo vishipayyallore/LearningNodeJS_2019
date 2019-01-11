de631d4b5c1d0eaeb04497fe02a46a9bc4978c98
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-01-11 23:21:05 +0530

    Passing Data to Event Handler

--- EventLoopDemo/app.js
+++ EventLoopDemo/app.js
@@ -3,18 +3,20 @@ var events = require('events');
 var eventEmitter = new events.EventEmitter();
 var eventName = 'data_received';
 var eventName1 = 'connection';
+var inputData = { Name: 'Shiva', Age: 25, Salary: 21345 };
 
 // Event Handler
 var connectionHandler = function connected(){
     console.log('Connection Successful');
 
-    eventEmitter.emit(eventName);
+    eventEmitter.emit(eventName, inputData);
 }
 
 eventEmitter.on(eventName1, connectionHandler);
 
-eventEmitter.on(eventName, function(){
-    console.log('data received succesfully.');
+eventEmitter.on(eventName, function(input){
+    console.log('----- data received succesfully -----');
+    console.log(`Person Details: ${input.Name} ${input.Age} ${input.Salary}`);
 });
 
 eventEmitter.emit(eventName1);
