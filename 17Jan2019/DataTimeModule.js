04b2d37b649ee8f0e08dff0f8c826f8ae7fd17db
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-01-17 09:44:06 +0530

    Simple MOdule

---
+++ 17Jan2019/DataTimeModule.js
@@ -0,0 +1,5 @@
+"use strict";
+
+exports.dateTimeISO = function () {
+    return new Date().toISOString();
+};
--- 17Jan2019/app.js
+++ 17Jan2019/app.js
@@ -1,13 +1,18 @@
-var datetime = new Date();
-console.log(datetime);
+var dt = require("./DataTimeModule");
+
 setInterval(function () {
     
-    /*
-    var dt = new Date(Date.now()).toLocaleString()
-    console.log(dt.toLocaleString());
-    */
-
-   var datetime = new Date();
-   console.log(datetime.toISOString());
+   console.log(dt.dateTimeISO());
 
 }, 1000);
+
+/*
+var datetime = new Date();
+console.log(datetime);
+
+    var dt = new Date(Date.now()).toLocaleString()
+    console.log(dt.toLocaleString());
+
+       var datetime = new Date();
+
+*/
